6951c7ba89015b3ded39b8fbf135358d82308ccd
mjHarakka <mj.harakka@gmail.com>
2019-09-04 11:34:26 +0300

    first commit

---
+++ src/main/java/com/mikkoharakka/bookstore/model/Book.java
@@ -0,0 +1,13 @@
+package com.mikkoharakka.bookstore.model;
+
+public class Book {
+	
+	// Is using lombok so should be no need for constructors, getters, setters
+	
+	private String title;
+	private String author;
+	private int year;
+	private int isbn;
+	private double price;
+
+}
