a4734bab0fc0c3fcbd63c843ff3878570f9594cb
mjHarakka <mj.harakka@gmail.com>
2019-09-16 20:31:46 +0300

    done part 1 of exercise

--- src/main/java/com/mikkoharakka/bookstore/controller/BookController.java
+++ src/main/java/com/mikkoharakka/bookstore/controller/BookController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mikkoharakka.bookstore.model.Book;
 import com.mikkoharakka.bookstore.repository.BookRepository;
 
 
@@ -15,6 +18,58 @@ public class BookController {
 	@Autowired
 	private BookRepository bookRepository;
 
+	@GetMapping("/addbook")
+	public String addBook() {
+		return "addbook";
+	}
+	
+	@PostMapping("/addbook")
+    public String assignAirport(@RequestParam String author,
+    		@RequestParam String title,
+    		@RequestParam int year,
+    		@RequestParam String isbn,
+    		@RequestParam double price) {
+    
+	Book b = new Book();
+	b.setAuthor(author);
+	b.setIsbn(isbn);
+	b.setTitle(title);
+	b.setPrice(price);
+	b.setYear(year);
+    
+	bookRepository.save(b);
+    
+    return "redirect:/addbook/";
+}
+	
+	@GetMapping("/edit/{id}")
+	public String editBook(@PathVariable long id, Model model) {
+		model.addAttribute("book", bookRepository.getOne(id));
+		return "edit";
+	}
+	
+	@PostMapping("/edit/{id}")
+	public String edit(@PathVariable long id,
+			@RequestParam String author,
+    		@RequestParam String title,
+    		@RequestParam int year,
+    		@RequestParam String isbn,
+    		@RequestParam double price) {
+		
+		Book b = bookRepository.getOne(id);
+		
+		b.setAuthor(author);
+		b.setIsbn(isbn);
+		b.setTitle(title);
+		b.setPrice(price);
+		b.setYear(year);
+		
+		bookRepository.deleteById(id);
+		bookRepository.save(b);
+		
+		return "redirect:/";
+	}
+	
 	@GetMapping("/listbooks/{id}")
 	public String deleteBook(@PathVariable long id) {
 		bookRepository.deleteById(id);
