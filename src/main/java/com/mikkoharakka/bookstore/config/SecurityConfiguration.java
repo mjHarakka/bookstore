f5dbf4e65daa71547d71b77d3c54afc82526aebd
mjHarakka <mj.harakka@gmail.com>
2019-10-06 18:05:11 +0300

    got the sec working properly

--- src/main/java/com/mikkoharakka/bookstore/config/SecurityConfiguration.java
+++ src/main/java/com/mikkoharakka/bookstore/config/SecurityConfiguration.java
@@ -1,8 +1,4 @@
 package com.mikkoharakka.bookstore.config;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +10,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -23,52 +21,41 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
+	// Another possible way to go: https://docs.spring.io/spring-security/site/docs/current/guides/html5/hellomvc-javaconfig.html
+	@Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("mikko").password(passwordEncoder().encode("sala")).roles("USER")
+          .and()
+          .withUser("admin").password(passwordEncoder().encode("sudo")).roles("ADMIN");
+    }
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-        .authorizeRequests()
-            .antMatchers("/", "/listbooks").permitAll()
-            .anyRequest().authenticated()
-            .and()
-        .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-        .logout()
-            .permitAll();
-	}
-		 
-	
-	@Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList();
-    	UserDetails user = User.withDefaultPasswordEncoder()
-                .username("mikko")
-                .password("sala")
-                .roles("USER")
-                .build();
-
-    	users.add(user);
-    	
-    	user = User.withDefaultPasswordEncoder()
-                   .username("admin")
-                   .password("sudo")
-                   .roles("USER", "ADMIN")
-                   .build();
-    	
-    	users.add(user);
-    	
-        return new InMemoryUserDetailsManager(users);
-    }
-
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        		.antMatchers("/h2-console/**").permitAll()
+        		.anyRequest().authenticated()
+        		.and()
+        	.formLogin()
+        		.loginPage("/login")
+        		.permitAll()
+        		.and()
+        	.logout()
+        		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        		.and()
+        	.httpBasic()
+        	.and().csrf().ignoringAntMatchers("/h2-console/**")
+        	.and().headers().frameOptions().sameOrigin();
+    }	
 	
 	@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
-
-
+ 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     
 }
